dd59ded46e79b3a2158a0e4696dd06a4bff2e343
OFFICE <office@OFFICEs-MacBook-Air-2.local>
2020-02-27 23:09:48 -0500

    rating range acceptable to book review

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -6,6 +6,6 @@ class BookReview < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :review, presence: true
-  validates :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, presence: true
   belongs_to :user
 end
